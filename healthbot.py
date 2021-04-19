c7eb46dd2bcaabe201b39d5c20b0276748774a1c
Dave <438764564@qq.com>
2021-04-19 16:36:38 +0800

    add welcome message

--- healthbot.py
+++ healthbot.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 import configparser
@@ -8,6 +8,7 @@ import os
 import requests
 
 global redis1
+global rapid_headers
 
 def main():
     # Load your token and create an Updater for your Bot
@@ -16,10 +17,15 @@ def main():
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-
+    
     global redis1
     redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
-
+    
+    global rapid_headers
+    rapid_headers = {
+            'x-rapidapi-key': (config['RAPIDAPI']['APIKEY']),
+            'x-rapidapi-host': "calorieninjas.p.rapidapi.com"
+            }
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
@@ -29,6 +35,7 @@ def main():
     dispatcher.add_handler(echo_handler)
 
     # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start_handler))
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("hello", hello))
@@ -45,6 +52,14 @@ def echo(update, context):
     logging.info("Update: " + str(update))
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+
+def start_handler(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    welcome_message = "Hi, my dear master!\n" \
+                    "Send me your weight! Let me record for you!\n" \
+                    "Want to search food info? /search is for you!"
+    reply_keyboard_markup = ReplyKeyboardMarkup([['/search egg'],['/search tomato']])
+    update.message.reply_text(welcome_message, reply_markup=reply_keyboard_markup)
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -74,22 +89,21 @@ url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
 
 def searchfood(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /hello is issued."""
-    outputstr = ""
-    inputstring  = context.args[0]
-    querystring = {"query":inputstring}
-    headers = {
-        'x-rapidapi-key': "10240eda73mshec0bc03cd897568p16cae8jsnd9d2285d965e",
-        'x-rapidapi-host': "calorieninjas.p.rapidapi.com"
-        }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    response = response.json()['items']
-    if (response == []): outputstr = "Sorry, I am not sure whether "+ inputstring + "is a kind of food or not."
-    else: 
-        res_data = response[0]
-        outputstr = inputstring + " contains " + str(res_data['sugar_g']) + "g sugar" + " and " + str(res_data['fiber_g']) + "g fiber" " as well as " + str(res_data['calories']) + " calories."
+    global rapid_headers
+    try:
+        outputstr = ""
+        inputstring  = context.args[0]
+        querystring = {"query":inputstring}
+        response = requests.request("GET", url, headers=rapid_headers, params=querystring)
+        response = response.json()['items']
+        if (response == []): outputstr = "Sorry, I am not sure whether "+ inputstring + " is a kind of food or not."
+        else: 
+            res_data = response[0]
+            outputstr = inputstring + " contains " + str(res_data['sugar_g']) + "g sugar" + " and " + str(res_data['fiber_g']) + "g fiber" " as well as " + str(res_data['calories']) + " calories."
 
-    update.message.reply_text(outputstr)
-
+        update.message.reply_text(outputstr)
+    except (IndexError, ValueError):
+        update.message.reply_text("Please type something followed by search command!")
 
 
 if __name__ == '__main__':
